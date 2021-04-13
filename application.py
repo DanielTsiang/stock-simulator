96cbadf75c5a973943c63be67d3d1943bf419ec1
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2021-04-13 17:33:45 +0000

    Revert "Revert "Change database url""
    This reverts commit fdc7e1725eec92a484ce5501bcf3a74ae8ad923d.

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use PostgreSQL database
-db = SQL(os.getenv("DATABASE_URL"))
+db = SQL(os.getenv("DATA_BASE_URL"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
