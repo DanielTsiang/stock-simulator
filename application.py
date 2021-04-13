007f397c5eae994d524e8239e70df5068807eec8
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2021-04-13 16:59:00 +0000

    added Procfile and changed SQL history.transacted to TEXT

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db", connect_args={"check_same_thread": False})
+db = SQL(os.getenv("DATABASE_URL"), connect_args={"check_same_thread": False})
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
