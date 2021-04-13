cf33ad9364a681d79bff40c9298bb7c95c7260e2
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2021-04-13 22:11:17 +0000

    added sqlalchemy and psycopg2, updated database url

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import os, time
+import os, time, sqlalchemy.dialects.sqlite, psycopg2
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
@@ -38,7 +38,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use PostgreSQL database
-db = SQL(os.getenv("DATABASE_URL"))
+db = SQL(os.getenv("DATA_BASE_URL"))
 
 # Make sure API key is set
 if not os.environ.get("API_KEY"):
