e9060afcb9cabceb4ffff563f1f20ffa8a171aa1
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2021-11-13 01:52:20 +0000

    Use temp dir and preload app

--- application.py
+++ application.py
@@ -40,6 +40,7 @@ app.jinja_env.filters["datetimeformat"] = datetimeformat
 
 
 # Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
