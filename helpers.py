a32dec5f646d08cef2d991391b209d0b4a51da00
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2022-07-02 13:56:59 +0000

    Add integrity checks for scripts loaded from CDNs

--- helpers.py
+++ helpers.py
@@ -2,7 +2,7 @@ import os
 import requests
 import urllib.parse
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, session
 from functools import wraps
 from datetime import datetime
 
