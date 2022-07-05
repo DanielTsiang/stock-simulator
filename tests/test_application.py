3463b5488823e16479b6d2e7ea5fe16619852733
Daniel Tsiang <dan_tsiang@hotmail.co.uk>
2022-07-05 22:21:34 +0000

    Add history endpoint unit tests

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from datetime import datetime, timezone
 from flask import template_rendered
 from http import HTTPStatus
 from pathlib import Path
@@ -19,9 +20,10 @@ NAME1 = "Alphabet Inc"
 NEW_PRICE = 2174.53
 PRICE = 2129.78
 SYMBOL1 = "GOOGL"
-TEST = "test"
 TEST1 = "test1"
-USER_ID = 2
+TEST2 = "test2"
+USER_ID1 = 1
+USER_ID2 = 2
 
 LOOKUP_RETURN = {
     "GOOGL": {
@@ -54,6 +56,7 @@ def setUp():
 
 env_patcher = setUp()
 from application import app, db
+import helpers
 
 
 @contextmanager
@@ -73,18 +76,18 @@ def captured_templates(app):
 class ApplicationTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.login_payload = {"username": TEST, "password": TEST}
+        cls.login_payload = {"username": TEST1, "password": TEST1}
 
         # Register user
-        payload = {"username": TEST, "password": TEST, "confirmation": TEST}
+        payload = {"username": TEST1, "password": TEST1, "confirmation": TEST1}
         with app.test_client() as test_client:
             test_client.post("/register", data=payload)
-        db.execute("UPDATE users SET id = ? WHERE username = ?", USER_ID, TEST)
+        db.execute("UPDATE users SET id = ? WHERE username = ?", USER_ID2, TEST1)
 
         # Insert buy log into history table
         db.execute(
             "INSERT INTO history (user_id, symbol, shares, price, transacted) VALUES (?, ?, ?, ?, datetime('now'))",
-            USER_ID,
+            USER_ID2,
             SYMBOL1,
             1,
             PRICE,
@@ -92,14 +95,14 @@ class ApplicationTest(unittest.TestCase):
         db.execute(
             "UPDATE history SET id = ? WHERE user_id = ? AND symbol = ? AND transacted = datetime('now')",
             HISTORY_ID,
-            USER_ID,
+            USER_ID2,
             SYMBOL1,
         )
 
         # Insert share into shares table
         db.execute(
             "INSERT INTO shares VALUES (?, ?, ?, ?, ?, ?)",
-            USER_ID,
+            USER_ID2,
             SYMBOL1,
             NAME1,
             1,
@@ -142,19 +145,19 @@ class ApplicationTest(unittest.TestCase):
 
     def test_post_register(self):
         # GIVEN
-        payload = {"username": TEST1, "password": TEST1, "confirmation": TEST1}
+        payload = {"username": TEST2, "password": TEST2, "confirmation": TEST2}
 
         # WHEN
         with app.test_client() as test_client:
             response = test_client.post("/register", data=payload)
-        username = db.execute("SELECT username FROM users WHERE username = ?", TEST1)[
+        username = db.execute("SELECT username FROM users WHERE username = ?", TEST2)[
             0
         ]["username"]
 
         # THEN
         self.assertEqual(HTTPStatus.FOUND, response.status_code)
         self.assertEqual("/", response.location)
-        self.assertEqual(TEST1, username)
+        self.assertEqual(TEST2, username)
 
     def test_get_logout(self):
         # WHEN
@@ -171,7 +174,7 @@ class ApplicationTest(unittest.TestCase):
             with captured_templates(app) as templates:
                 # Mock user logged in
                 with test_client.session_transaction() as session:
-                    session["user_id"] = USER_ID
+                    session["user_id"] = USER_ID2
                 # WHEN
                 response = test_client.get("/")
                 template, context = templates[0]
@@ -194,14 +197,14 @@ class ApplicationTest(unittest.TestCase):
                     "shares_count": 1,
                     "symbol": SYMBOL1,
                     "total": NEW_PRICE,
-                    "user_id": USER_ID,
+                    "user_id": USER_ID2,
                 }
             ],
         }
 
         expected_shares = [
             {
-                "user_id": USER_ID,
+                "user_id": USER_ID2,
                 "symbol": SYMBOL1,
                 "name": NAME1,
                 "shares_count": 1,
@@ -214,11 +217,11 @@ class ApplicationTest(unittest.TestCase):
         with app.test_client() as test_client:
             # Mock user logged in
             with test_client.session_transaction() as session:
-                session["user_id"] = USER_ID
+                session["user_id"] = USER_ID2
             response = test_client.get("/index_json")
 
         updated_shares = db.execute(
-            "SELECT * FROM shares WHERE user_id = ? AND symbol = ?", USER_ID, SYMBOL1
+            "SELECT * FROM shares WHERE user_id = ? AND symbol = ?", USER_ID2, SYMBOL1
         )
 
         # THEN
@@ -234,7 +237,7 @@ class ApplicationTest(unittest.TestCase):
         with app.test_client() as test_client:
             # Mock user logged in
             with test_client.session_transaction() as session:
-                session["user_id"] = USER_ID
+                session["user_id"] = USER_ID2
             response = test_client.get("/select_json", query_string=payload)
 
         # THEN
@@ -260,7 +263,7 @@ class ApplicationTest(unittest.TestCase):
         with app.test_client() as test_client:
             # Mock user logged in
             with test_client.session_transaction() as session:
-                session["user_id"] = USER_ID
+                session["user_id"] = USER_ID2
             response = test_client.put("/buy", data=payload)
 
         # THEN
@@ -277,12 +280,74 @@ class ApplicationTest(unittest.TestCase):
         with app.test_client() as test_client:
             # Mock user logged in
             with test_client.session_transaction() as session:
-                session["user_id"] = USER_ID
+                session["user_id"] = USER_ID2
             response = test_client.get("/buyCheck", query_string=payload)
 
         # THEN
         self.assertEqual(HTTPStatus.OK, response.status_code)
         self.assertEqual(True, response.json)
+
+    def test_get_history(self):
+        # GIVEN
+        with app.test_client() as test_client:
+            with captured_templates(app) as templates:
+                # Mock user logged in
+                with test_client.session_transaction() as session:
+                    session["user_id"] = USER_ID2
+                # WHEN
+                response = test_client.get("/history")
+                template, context = templates[0]
+
+        # THEN
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        self.assertEqual("history.html", template.name)
+
+    def test_delete_history(self):
+        # GIVEN
+        with app.test_client() as test_client:
+            # Mock user logged in
+            with test_client.session_transaction() as session:
+                session["user_id"] = USER_ID1
+            # WHEN
+            response = test_client.delete("/history")
+
+        # THEN
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        self.assertEqual(True, response.json)
+
+        # Obtain history information for logged-in user
+        transactions = db.execute(
+            "SELECT * FROM history WHERE user_id = ? ORDER BY transacted DESC", USER_ID1
+        )
+        self.assertEqual([], transactions)
+
+    def test_get_history_json(self):
+        # GIVEN
+        expected_history = {
+            "data": [
+                {
+                    "id": HISTORY_ID,
+                    "price": helpers.usd(PRICE),
+                    "shares": 1,
+                    "symbol": SYMBOL1,
+                    "transacted": datetime.now(timezone.utc).strftime(
+                        "%d-%m-%Y %H:%M:%S"
+                    ),
+                    "user_id": USER_ID2,
+                }
+            ]
+        }
+
+        # WHEN
+        with app.test_client() as test_client:
+            # Mock user logged in
+            with test_client.session_transaction() as session:
+                session["user_id"] = USER_ID2
+            response = test_client.get("/history_json")
+
+        # THEN
+        self.assertEqual(HTTPStatus.OK, response.status_code)
+        self.assertEqual(expected_history, response.json)
 
 
 if __name__ == "__main__":
