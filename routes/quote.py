52c2a2b79d0dd2a5386f9b5c5970459d87d66a5e
Sourcery AI <>
2023-06-06 23:14:53 +0000

    'Refactored by Sourcery'

--- routes/quote.py
+++ routes/quote.py
@@ -10,19 +10,11 @@ quote_blueprint = Blueprint("quote", __name__)
 def quote():
     """Get stock quote."""
 
-    # Access form data
-    symbol = request.args.get("symbol_quote")
-
-    # Ensure quote symbol was submitted
-    if not symbol:
+    if symbol := request.args.get("symbol_quote"):
+        return (
+            jsonify(QUOTED=quoted, symbol=symbol)
+            if (quoted := lookup(symbol))
+            else apology("invalid symbol", 400)
+        )
+    else:
         return apology("must provide quote", 400)
-
-    # Obtain quote using lookup function
-    quoted = lookup(symbol)
-
-    # Invalid symbol provided
-    if not quoted:
-        return apology("invalid symbol", 400)
-
-    # Return quoted information
-    return jsonify(QUOTED=quoted, symbol=symbol)
